1bdeebac7679a7df2112375c6f276407137678e6
Renato Semadeni <renato.semadeni@gmail.com>
2018-02-13 06:31:24 +0100

    shaders added

---
+++ Engine/src/colorshaderclass.cpp
@@ -0,0 +1,149 @@
+#include "colorshaderclass.h"
+
+ColorShaderClass::ColorShaderClass()
+{
+	m_vertexShader = 0;
+	m_pixelShader = 0;
+	m_layout = 0;
+	m_matrixBuffer = 0;
+}
+
+ColorShaderClass::ColorShaderClass(const ColorShaderClass& other)
+{
+}
+
+ColorShaderClass::~ColorShaderClass()
+{
+}
+
+bool ColorShaderClass::Initialize(ID3D11Device* device, HWND hwnd)
+{
+	bool result;
+
+	// initialize the vertex and pixel shaders
+	result = InitializeShader(device, hwnd, L"./shader/color.vs.hlsl", L"./shader/color.ps.hlsl");
+	if (!result) {
+		return false;
+	}
+	return true;
+}
+
+void ColorShaderClass::Shutdown()
+{
+	// Shutdown the vertex and pixel shaders as well as the related objects
+	ShutdownShader();
+
+	return;
+}
+
+bool ColorShaderClass::Render(ID3D11DeviceContext* deviceContext, int indexCount, XMMATRIX worldMatrix, XMMATRIX viewMatrix, XMMATRIX projectionMatrix)
+{
+	bool result;
+
+	// set the shader parameters that it will use for rendering
+	result = SetShaderParameters(deviceContext, worldMatrix, viewMatrix, projectionMatrix);
+	if (!result) {
+		return false;
+	}
+
+	// now render the prepared buffers with the shader
+	RenderShader(deviceContext, indexCount);
+
+	return true;
+}
+
+bool ColorShaderClass::InitializeShader(ID3D11Device* device, HWND hwnd, WCHAR* vsFilename, WCHAR* psFilename)
+{
+	HRESULT result;
+	ID3D10Blob* errorMessage;
+	ID3D10Blob* vertexShaderBuffer;		// contains the compiled vs shader
+	ID3D10Blob* pixelShaderBuffer;		// contains the compiled ps shader
+	D3D11_INPUT_ELEMENT_DESC polygonLayout[2];	// vertex and color
+	unsigned int numElements;
+	D3D11_BUFFER_DESC matrixBufferDesc;
+
+	// initialize the pointers
+	errorMessage = nullptr;
+	vertexShaderBuffer = nullptr;
+	pixelShaderBuffer = nullptr;
+
+	// compile the VERTEX SHADER code
+	result = D3DCompileFromFile(vsFilename, NULL, NULL, "ColorVertexShader", "vs_5_0", 
+		D3D10_SHADER_ENABLE_STRICTNESS, 0, &vertexShaderBuffer, &errorMessage);
+	if (FAILED(result)) {
+		// if the shader failed to compile it has written something into the error message
+		if (errorMessage) {
+			OutputShaderErrorMessage(errorMessage, hwnd, vsFilename);
+		}
+		// if there is nothing in the error message then simply could not find the shader itself
+		else {
+			MessageBox(hwnd, vsFilename, L"Missing Shader File", MB_OK);
+		}
+		return false;
+	}
+
+	// compile the PIXEL SHADER code
+	result = D3DCompileFromFile(psFilename, NULL, NULL, "ColorPixelShader", "vs_5_0",
+		D3D10_SHADER_ENABLE_STRICTNESS, 0, &pixelShaderBuffer, &errorMessage);
+	if (FAILED(result)) {
+		// if the shader failed to compile it has written something into error message
+		if (errorMessage) {
+			OutputShaderErrorMessage(errorMessage, hwnd, psFilename);
+		}
+		// if there is nothing then simply could not find the file itself
+		else {
+			MessageBox(hwnd, psFilename, L"Missing Shader File", MB_OK);
+		}
+		return false;
+	}
+
+	// create the vertex shader from the buffer
+	result = device->CreateVertexShader(vertexShaderBuffer->GetBufferPointer(),
+		vertexShaderBuffer->GetBufferSize(), NULL, &m_vertexShader);
+	if (FAILED(result)) {
+		return false;
+	}
+
+	// create the pixel shader from the buffer
+	result = device->CreatePixelShader(pixelShaderBuffer->GetBufferPointer(),
+		pixelShaderBuffer->GetBufferSize(), NULL, &m_pixelShader);
+	if (FAILED(result)) {
+		return false;
+	}
+
+	// create the vertex input layout description
+	//  this setup needs to match the VertexType structure in the ModelClass and Shader
+	polygonLayout[0].SemanticName = "POSITION";		// must match shader variable name
+	polygonLayout[0].SemanticIndex = 0;
+	polygonLayout[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	polygonLayout[0].InputSlot = 0;
+	polygonLayout[0].AlignedByteOffset = 0;
+	polygonLayout[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[0].InstanceDataStepRate = 0;
+
+	polygonLayout[1].SemanticName = "COLOR";		// must match shader variable name
+	polygonLayout[1].SemanticIndex = 0;
+	polygonLayout[1].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	polygonLayout[1].InputSlot = 0;
+	polygonLayout[1].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+	polygonLayout[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[1].InstanceDataStepRate = 0;
+
+	// get a count of elements in the layout
+	numElements = sizeof(polygonLayout) / sizeof(polygonLayout[0]);
+
+	// create the vertex input layout
+	result = device->CreateInputLayout(polygonLayout, numElements, 
+		vertexShaderBuffer->GetBufferPointer(), vertexShaderBuffer->GetBufferSize(), 
+		&m_layout);
+	if (FAILED(result)) {
+		return false;
+	}
+
+	// release the vertex shader buffer and pixel shader buffer since not needed any longer
+	vertexShaderBuffer->Release();
+	vertexShaderBuffer = nullptr;
+
+	pixelShaderBuffer->Release();
+	pixelShaderBuffer = nullptr;
+}
