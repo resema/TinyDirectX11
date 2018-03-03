acf28fef450e9d74e4bc090ac627dd6a607354a9
Renato Semadeni <renato.semadeni@gmail.com>
2018-03-03 08:53:09 +0100

    polygonlayout created

--- Engine/include/lightshaderclass.h
+++ Engine/include/lightshaderclass.h
@@ -2,6 +2,7 @@
 #define LIGHTSHADERCLASS_H
 
 #include <d3d11.h>
+#include <d3dcompiler.h>
 #include <DirectXMath.h>
 using namespace DirectX;
 
--- Engine/src/lightshaderclass.cpp
+++ Engine/src/lightshaderclass.cpp
@@ -63,3 +63,148 @@ bool LightShaderClass::Render(ID3D11DeviceContext* deviceContext, int indexCount
 
 	return true;
 }
+
+bool LightShaderClass::InitializeShader(ID3D11Device* device, HWND hwnd,
+	WCHAR* vsFilename, WCHAR* psFilename)
+{
+	HRESULT result;
+	ID3D10Blob* errorMessage;
+	ID3D10Blob* vertexShaderBuffer;
+	ID3D10Blob* pixelShaderBuffer;
+
+	D3D11_INPUT_ELEMENT_DESC polygonLayout[3];
+	unsigned int numElements;
+	D3D11_SAMPLER_DESC samplerDesc;
+	D3D11_BUFFER_DESC matrixBufferDesc;
+	D3D11_BUFFER_DESC lightBufferDesc;
+
+	// initialize the pointers
+	errorMessage = nullptr;
+	vertexShaderBuffer = nullptr;
+	pixelShaderBuffer = nullptr;
+
+	// compile the vertex shader code
+	result = D3DCompileFromFile(
+		vsFilename,							// filename
+		NULL,								// ptr to array of macros
+		NULL,								// ptr to an include interface
+		"LightVertexShader",				// name of the shader function
+		"vs_5_0",							// version of the shader
+		D3D10_SHADER_ENABLE_STRICTNESS,		// compile flags
+		0,									// effect flags
+		&vertexShaderBuffer,				// compiled shader
+		&errorMessage						// lists of errors and warnings
+		);
+	if (FAILED(result))
+	{
+		// if the shader failed to compile it should have written somthing to error msg
+		if (errorMessage)
+		{
+			OutputShaderErrorMessage(errorMessage, hwnd, vsFilename);
+		}
+		// if there was nothing in the error msg then it simply could not find the shader file
+		else
+		{
+			MessageBox(hwnd, vsFilename, L"Missing Vertex Shader File", MB_OK);
+		}
+
+		return false;
+	}
+
+	// compile the pixel shader code
+	result = D3DCompileFromFile(
+		psFilename,							// filename
+		NULL,								// ptr to array of macros
+		NULL,								// ptr to an include interface
+		"LightPixelShader",					// name of the shader function
+		"ps_5_0",							// version of the shader
+		D3D10_SHADER_ENABLE_STRICTNESS,		// compile flags
+		0,									// effect flags
+		&pixelShaderBuffer,					// compiled shader
+		&errorMessage						// lists of errors and warnings
+		);
+	if (FAILED(result))
+	{
+		// if the shader failed to compile it should have written somthing to error msg
+		if (errorMessage)
+		{
+			OutputShaderErrorMessage(errorMessage, hwnd, psFilename);
+		}
+		// if there was nothing in the error msg then it simply could not find the shader file
+		else
+		{
+			MessageBox(hwnd, psFilename, L"Missing Pixel Shader File", MB_OK);
+		}
+
+		return false;
+	}
+
+	// create the vertex shader from the buffer
+	result = device->CreateVertexShader(
+		vertexShaderBuffer->GetBufferPointer(),
+		vertexShaderBuffer->GetBufferSize(),
+		NULL,
+		&m_vertexShader
+		);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// create the pixel shader from the buffer
+	result = device->CreatePixelShader(
+		pixelShaderBuffer->GetBufferPointer(),
+		pixelShaderBuffer->GetBufferSize(),
+		NULL,
+		&m_pixelShader
+		);
+
+	// create the vertex input layout description
+	// this setup needs to match the vertextype structure in the ModelClass and shader
+	polygonLayout[0].SemanticName = "POSITION";
+	polygonLayout[0].SemanticIndex = 0;
+	polygonLayout[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	polygonLayout[0].InputSlot = 0;
+	polygonLayout[0].AlignedByteOffset = 0;
+	polygonLayout[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[0].InstanceDataStepRate = 0;
+
+	polygonLayout[1].SemanticName = "TEXCOORD";
+	polygonLayout[1].SemanticIndex = 0;
+	polygonLayout[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+	polygonLayout[1].InputSlot = 0;
+	polygonLayout[1].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+	polygonLayout[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[1].InstanceDataStepRate = 0;
+
+	polygonLayout[2].SemanticName = "NORMAL";
+	polygonLayout[2].SemanticIndex = 0;
+	polygonLayout[2].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	polygonLayout[2].InputSlot = 0;
+	polygonLayout[2].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+	polygonLayout[2].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[2].InstanceDataStepRate = 0;
+
+	// get a count of the elements in the layout
+	numElements = sizeof(polygonLayout) / sizeof(polygonLayout[0]);
+
+	// create the vertex input layout
+	result = device->CreateInputLayout(
+		polygonLayout,
+		numElements,
+		vertexShaderBuffer->GetBufferPointer(),
+		vertexShaderBuffer->GetBufferSize(),
+		&m_layout
+		);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// release the vertex shader buffer and pixel shader - not used anymore
+	vertexShaderBuffer->Release();
+	vertexShaderBuffer = nullptr;
+
+	pixelShaderBuffer->Release();
+	pixelShaderBuffer = nullptr;
+}
