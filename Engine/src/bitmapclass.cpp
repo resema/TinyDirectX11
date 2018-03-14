6dd6e0c46ea2c6e0dbef90dbbfd09c6726bee344
Renato Semadeni <renato.semadeni@gmail.com>
2018-03-14 17:12:04 +0100

    2d Rendering / sprites started

---
+++ Engine/src/bitmapclass.cpp
@@ -0,0 +1,22 @@
+#include "bitmapclass.h"
+
+BitmapClass::BitmapClass()
+{
+	m_vertexBuffer = nullptr;
+	m_indexBuffer = nullptr;
+	m_Texture = nullptr;
+}
+
+BitmapClass::BitmapClass(const BitmapClass& other)
+{
+}
+
+BitmapClass::~BitmapClass()
+{
+}
+
+bool BitmapClass::Initialize(ID3D11Device* device, int screenWidth, int screenHeight,
+	WCHAR* textureFilename, int bitmapWidth, int bitmapHeight)
+{
+
+}
