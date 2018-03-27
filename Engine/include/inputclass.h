048f581d566ae836dc46e0aaa953c6ce10336664
Renato Semadeni <renato.semadeni@gmail.com>
2018-03-27 17:20:05 +0200

    started DirectInput

--- Engine/include/inputclass.h
+++ Engine/include/inputclass.h
@@ -1,22 +1,47 @@
 #ifndef INPUTCLASS_H
 #define INPUTCLASS_H
 
+// pre-processing directives
+#define DIRECTINPUT_VERSION 0x0800
+
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
+
+#include <dinput.h>
+
 class InputClass
 {
 public:
 	InputClass();
 	InputClass(const InputClass&);
 	~InputClass();
+	// rule of five
+	InputClass& operator=(const InputClass&) = default;
+	InputClass(InputClass&&) = default;
+	InputClass& operator=(InputClass&&) = default;
 
-	void Initialize();
+	bool Initialize(HINSTANCE, HWND, int, int);
+	void Shutdown();
+	bool Frame();
 
-	void KeyDown(unsigned int);
-	void KeyUp(unsigned int);
-
-	bool IsKeyDown(unsigned int);
+	bool IsEscapePressed();
+	void GetMouseLocation(int&, int&);
 
 private:
-	bool m_keys[256];
+	bool ReadKeyboard();
+	bool ReadMouse();
+	void ProcessInput();
+
+private:
+	IDirectInput8 * m_directInput;
+	IDirectInputDevice8* m_keyboard;
+	IDirectInputDevice8* m_mouse;
+
+	unsigned char m_keyboardState[256];
+	DIMOUSESTATE m_mouseState;
+
+	int m_screenWidth, m_screenHeight;
+	int m_mouseX, m_mouseY;
 };
 
 #endif	// INPUTCLASS_H
--- Engine/src/inputclass.cpp
+++ Engine/src/inputclass.cpp
@@ -1,6 +1,7 @@
 #include "inputclass.h"
 
 InputClass::InputClass()
+	: m_directInput(nullptr), m_keyboard(nullptr), m_mouse(nullptr)
 {
 }
 
@@ -12,17 +13,50 @@ InputClass::~InputClass()
 {
 }
 
-void InputClass::Initialize()
+bool InputClass::Initialize(HINSTANCE hinstance, HWND hwnd, int screenWidth, int screenHeight)
 {
-	int i;
+	HRESULT result;
 
-	// initialize all keys to being release and not pressed
-	for (i = 0; i < 256; i++)
+	// store the screen size which will be used for positioning the mosue cursor
+	m_screenWidth = screenWidth;
+	m_screenHeight = screenHeight;
+
+	// initialize the location of the mouse on the screen
+	m_mouseX = 0;
+	m_mouseY = 0;
+
+	// initialize the main direct input interface
+	result = DirectInput8Create(
+		hinstance,					// instance handle
+		DIRECTINPUT_VERSION,		// DirectInput version nbr
+		IID_IDirectInput8,			// Unique identifier of the desired interface
+		(void**)&m_directInput,		// ptr to variable to receive the interface ptr
+		NULL						// ptr to address of controlling object
+		);
+	if (FAILED(result))
 	{
-		m_keys[i] = false;
+		return false;
 	}
 
-	return;
+	// initialize the direct input interface for the KEYBOARD
+	result = m_directInput->CreateDevice(
+		GUID_SysKeyboard,		// ref to the GUID for desired input device
+		&m_keyboard,			// addr of variable to receive interface ptr
+		NULL					// addr of controlling object
+		);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// set the data format. in this case since it is a keyboard we can use the predefined data format
+	result = m_keyboard->SetDataFormat(
+		&c_dfDIKeyboard			// addr of struct that describes format of data
+		);
+	if (FAILED(result))
+	{
+		return false;
+	}
 }
 
 void InputClass::KeyDown(unsigned int input)
