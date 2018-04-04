76b53ebcb4b3032c82ee6d66786340a03ca1d369
Renato Semadeni <renato.semadeni@gmail.com>
2018-04-04 06:31:43 +0200

    started frustum culling

---
+++ Engine/include/frustumclass.h
@@ -0,0 +1,29 @@
+#ifndef FRUSTUMCLASS_H
+#define FRUSTUMCLASS_H
+
+#include <DirectXMath.h>
+using namespace DirectX;
+
+class FrustumClass
+{
+public:
+	FrustumClass() = default;
+	FrustumClass(const FrustumClass&) = default;
+	~FrustumClass() = default;
+	// rule of five
+	FrustumClass& operator=(const FrustumClass&) = default;
+	FrustumClass(FrustumClass&&) = default;
+	FrustumClass& operator=(FrustumClass&&) = default;
+
+	void ConstructFrustum(float, XMMATRIX, XMMATRIX);
+
+	bool CheckPoint(float, float, float);
+	bool CheckCube(float, float, float, float);
+	bool CheckSphere(float, float, float, float);
+	bool CheckRectangle(float, float, float, float, float, float);
+
+private:
+	XMVECTOR m_planes[6];
+};
+
+#endif	// FRUSTUMCLASS_H
